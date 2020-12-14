392ae6a048c286bfc2c9a1a3a8c339f55c1fef6f
git-gsxl <qq772262624@163.com>
2020-12-14 21:04:39 +0800

    1214

--- 0-java_project/src/com/ppl/helloword/demo/For_1212.java
+++ 0-java_project/src/com/ppl/helloword/demo/For_1212.java
@@ -6,4 +6,37 @@ package com.ppl.helloword.demo;
 */
 
 public class For_1212 {
+    public static void main(String[] args) {
+        int j_num = 0;
+        int o_num = 0;
+        int res_num = 0;
+        int count = 1;
+        int res = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 1) {
+                j_num += i;
+            } else {
+                o_num += i;
+            }
+            res_num += i;
+            if (i / 7 == count) {
+                count += 1;
+                res += i;
+            }
+        }
+        System.out.println("1-100奇数总和为：" + j_num);
+        System.out.println("1-100偶数总和为：" + o_num);
+        System.out.println("1-100总和为：" + res_num);
+        System.out.println("1-100是7的倍数的整数的个数：" + count);
+        System.out.println("1-100是7的倍数的整数的总和：" + res);
+        int age = 0;
+        while (true) {
+            age += 1;
+            System.out.println(age);
+            if (age == 3){
+                System.out.println("break");
+                break;
+            }
+        }
+    }
 }
